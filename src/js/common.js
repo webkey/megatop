1028c92202673ecb2072e5554f74f2805b5d4599
webkey <serhiy_i@ukr.net>
2018-04-10 17:09:57 +0300

    office_20180410_1705

--- src/js/common.js
+++ src/js/common.js
@@ -296,9 +296,23 @@ function slidersInit() {
 						}
 					},
 					{
-						breakpoint: 1024,
+						breakpoint: 1200,
 						settings: {
 							slidesToShow: 4,
+							slidesToScroll: 2,
+						}
+					},
+					{
+						breakpoint: 768,
+						settings: {
+							slidesToShow: 3,
+							slidesToScroll: 2,
+						}
+					},
+					{
+						breakpoint: 480,
+						settings: {
+							slidesToShow: 2,
 							slidesToScroll: 2,
 						}
 					}
@@ -340,6 +354,20 @@ function slidersInit() {
 						settings: {
 							slidesToShow: 3,
 							slidesToScroll: 2,
+						}
+					},
+					{
+						breakpoint: 960,
+						settings: {
+							slidesToShow: 2,
+							slidesToScroll: 2,
+						}
+					},
+					{
+						breakpoint: 640,
+						settings: {
+							slidesToShow: 1,
+							slidesToScroll: 1,
 						}
 					}
 				]
