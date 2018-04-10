d8f85f5808cd69a660f3413ae73eb0fb01430165
webkey <serhiy_i@ukr.net>
2018-04-10 12:39:11 +0300

    office_20180410_1235

--- src/js/common.js
+++ src/js/common.js
@@ -289,14 +289,14 @@ function slidersInit() {
 				arrows: true,
 				responsive: [
 					{
-						breakpoint: 1600,
+						breakpoint: 1280,
 						settings: {
 							slidesToShow: 5,
 							slidesToScroll: 2,
 						}
 					},
 					{
-						breakpoint: 1440,
+						breakpoint: 1024,
 						settings: {
 							slidesToShow: 4,
 							slidesToScroll: 2,
