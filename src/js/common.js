df5f2c50bfdc55257e78244f452cbf8424885b7d
webkey <serhiy_i@ukr.net>
2018-04-24 16:21:39 +0300

    office_20180424_1620

--- src/js/common.js
+++ src/js/common.js
@@ -2015,6 +2015,27 @@ function contactsMap() {
 	}
 }
 
+/**only number input*/
+function onlyNumberInput() {
+	// link: https://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
+
+	$("[data-only-number]").keydown(function (e) {
+		// Allow: backspace, delete, tab, escape, enter and .
+		if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+			// Allow: Ctrl+A, Command+A
+			(e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
+			// Allow: home, end, left, right, down, up
+			(e.keyCode >= 35 && e.keyCode <= 40)) {
+			// let it happen, don't do anything
+			return;
+		}
+		// Ensure that it is a number and stop the keypress
+		if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+			e.preventDefault();
+		}
+	});
+}
+
 /**
  * !Product order calculation
  * */
